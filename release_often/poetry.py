e43c78ca03844d6eb0bd92b26b8dba4ced3bd390
Brett Cannon <brett@python.org>
2020-03-27 14:42:11 -0700

    Finish tests for  poetry

--- release_often/poetry.py
+++ release_often/poetry.py
@@ -1,13 +1,18 @@
 import tomlkit
 
+
 def version_file_path(directory):
     """Verify pyproject.toml contains "tool.poetry"."""
     pyproject = directory / "pyproject.toml"
     if not pyproject.exists():
-        raise TypeError(f"{pyproject} does not exist")
+        raise TypeError(f"{pyproject!r} does not exist")
     data = tomlkit.loads(pyproject.read_text(encoding="utf-8"))
-    if "tool" not in data or "poetry" not in data["tool"]:
-        raise ValueError(f"{pyproject} missing a 'tool.poetry' table")
+    try:
+        data["tool"]["poetry"]["version"]
+    except KeyError as exc:
+        raise ValueError(
+            f"{pyproject!r} missing a 'tool.poetry' table with 'version' key"
+        ) from exc
     return pyproject
 
 
@@ -18,7 +23,7 @@ def read_version(file_contents):
     return details["version"]
 
 
-def change_version(file_contents, new_version):
+def change_version(file_contents, old_version, new_version):
     """Create new file contents for pyproject.toml with the new version."""
     data = tomlkit.loads(file_contents)
     details = data["tool"]["poetry"]
--- tests/test_poetry.py
+++ tests/test_poetry.py
@@ -1,8 +1,9 @@
 import pathlib
 
 import pytest
+import tomlkit
 
-from release_me import poetry
+from release_often import poetry
 
 
 @pytest.fixture
@@ -10,16 +11,47 @@ def data_path():
     return pathlib.Path(__file__).parent / "data"
 
 
-def test_no_pyproject(data_path):
-    assert poetry.version_file_path(data_path) is None
+@pytest.fixture
+def pyproject_contents(data_path):
+    path = data_path / "poetry" / "pyproject.toml"
+    return path.read_text(encoding="utf-8")
 
 
-def test_missing_poetry_details(data_path):
-    flit_path = data_path / "flit"
-    assert poetry.version_file_path(flit_path) is None
+class TestVersionFilePath:
+
+    """Tests for release_often.poetry.version_file_path()."""
+
+    def test_no_pyproject(self, data_path):
+        with pytest.raises(TypeError):
+            poetry.version_file_path(data_path)
 
 
-def test_success(data_path):
-    poetry_path = data_path / "poetry"
-    pyproject_path = poetry_path / "pyproject.toml"
-    assert poetry.version_file_path(poetry_path) == pyproject_path
+    def test_missing_poetry_details(self, data_path):
+        flit_path = data_path / "flit" / "top_pkg"
+        with pytest.raises(ValueError):
+            poetry.version_file_path(flit_path)
+
+
+    def test_success(self, data_path):
+        poetry_path = data_path / "poetry"
+        pyproject_path = poetry_path / "pyproject.toml"
+        assert poetry.version_file_path(poetry_path) == pyproject_path
+
+
+class TestReadVersion:
+
+    """Tests for release_often.poetry.read_version()."""
+
+    def test_success(self, pyproject_contents):
+        assert poetry.read_version(pyproject_contents) == "1.2.3"
+
+
+class TestChangeVersion:
+
+    """Tests for release_often.poetry.change_version()."""
+
+    def test_success(self, pyproject_contents):
+        new_version = "2.0.0"
+        changed = poetry.change_version(pyproject_contents, "1.2.3", new_version)
+        changed_data = tomlkit.loads(changed)
+        assert changed_data["tool"]["poetry"]["version"] == new_version
